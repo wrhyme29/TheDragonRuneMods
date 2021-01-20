5c027bcaf80346fbab832364043e138cb202d1f8
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-01-19 20:41:38 -0600

    Added Innate Power Controller + Tests

--- Controller/Heroes/Conman/CharacterCards/ConmanCharacterCardController.cs
+++ Controller/Heroes/Conman/CharacterCards/ConmanCharacterCardController.cs
@@ -11,11 +11,38 @@ namespace TheDragonRune.Conman
 		public ConmanCharacterCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
 		{
 		}
+		private ITrigger _reduceTrigger;
 		public override IEnumerator UsePower(int index = 0)
 		{
-			
+			//The next time {Conman} would take Damage, redirect it to another  hero target and reduce the damage by 2.
+			int reducePowerNumeral = GetPowerNumeral(0, 2);
+
+			int[] powerNumerals = new int[]
+			{
+				reducePowerNumeral
+			};
+			OnDealDamageStatusEffect onDealDamageStatusEffect = new OnDealDamageStatusEffect(CardWithoutReplacements, "RedirectAndLowerResponse", $"The next time {CharacterCard.Title} would take Damage, redirect it to another  hero target and reduce the damage by 2.", new TriggerType[]
+			{
+			TriggerType.RedirectDamage,
+			TriggerType.ReduceDamage
+			}, base.TurnTaker, base.Card, powerNumerals);
+			onDealDamageStatusEffect.TargetCriteria.IsSpecificCard = CharacterCard;
+			onDealDamageStatusEffect.DamageAmountCriteria.GreaterThan = 0;
+			onDealDamageStatusEffect.NumberOfUses = 1;
+			onDealDamageStatusEffect.BeforeOrAfter = BeforeOrAfter.Before;
+			IEnumerator coroutine = AddStatusEffect(onDealDamageStatusEffect);
+			if (base.UseUnityCoroutines)
+			{
+				yield return base.GameController.StartCoroutine(coroutine);
+			}
+			else
+			{
+				base.GameController.ExhaustCoroutine(coroutine);
+			}
 			yield break;
 		}
+
+
 		public override IEnumerator UseIncapacitatedAbility(int index)
 		{
 			switch (index)
@@ -34,6 +61,48 @@ namespace TheDragonRune.Conman
 						break;
 					}
 			}
+			yield break;
+		}
+
+		public IEnumerator RedirectAndLowerResponse(DealDamageAction dd, TurnTaker hero, StatusEffect effect, int[] powerNumerals = null)
+		{
+			int? num = null;
+			if (powerNumerals != null)
+			{
+				num = powerNumerals.ElementAtOrDefault(0);
+			}
+			if (!num.HasValue)
+			{
+				num = 2;
+			}
+
+            List<SelectCardDecision> storedResults = new List<SelectCardDecision>();
+			IEnumerator coroutine = GameController.SelectCardAndStoreResults(HeroTurnTakerController, SelectionType.RedirectDamage, new LinqCardCriteria(c => c.IsHero && c.IsTarget && c != Card && c.IsInPlayAndHasGameText, "hero target"), storedResults, false, cardSource: GetCardSource());
+			if (base.UseUnityCoroutines)
+			{
+				yield return base.GameController.StartCoroutine(coroutine);
+			}
+			else
+			{
+				base.GameController.ExhaustCoroutine(coroutine);
+			}
+			if(DidSelectCard(storedResults))
+            {
+				Card target = GetSelectedCard(storedResults);
+				IEnumerator redirect = base.GameController.RedirectDamage(dd, target, cardSource: GetCardSource());
+				IEnumerator reduce = base.GameController.ReduceDamage(dd, num.Value, _reduceTrigger, GetCardSource());
+				if (base.UseUnityCoroutines)
+				{
+					yield return base.GameController.StartCoroutine(redirect);
+					yield return base.GameController.StartCoroutine(reduce);
+				}
+				else
+				{
+					base.GameController.ExhaustCoroutine(redirect);
+					base.GameController.ExhaustCoroutine(reduce);
+				}
+			}
+			
 			yield break;
 		}
 	}
--- Testing/Heroes/ConmanTests.cs
+++ Testing/Heroes/ConmanTests.cs
@@ -34,5 +34,25 @@ namespace TheDragonRuneTest
             Assert.AreEqual(31, conman.CharacterCard.HitPoints);
         }
 
+        [Test()]
+        public void TestConmanInnatePower()
+        {
+            SetupGameController("BaronBlade", "TheDragonRune.Conman", "Legacy", "Bunker", "TheScholar", "Megalopolis");
+            StartGame();
+            DestroyNonCharacterVillainCards();
+            //The next time {Conman} would take Damage, redirect it to another  hero target and reduce the damage by 2.
+            UsePower(conman.CharacterCard);
+            SelectCardsForNextDecision(bunker.CharacterCard);
+            QuickHPStorage(baron, conman, legacy, bunker, scholar);
+            DealDamage(baron, conman, 5, DamageType.Melee);
+            QuickHPCheck(0, 0, 0, -3, 0);
+
+            //only next damage
+            QuickHPUpdate();
+            DealDamage(baron, conman, 5, DamageType.Melee);
+            QuickHPCheck(0, -5, 0, 0, 0);
+
+        }
+
     }
 }
