09197bf088541dd2db5a2cc9b1838b7b9c06bad5
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-01-19 22:35:45 -0600

    Added EarthIllusion Tests

--- Testing/Heroes/ConmanTests.cs
+++ Testing/Heroes/ConmanTests.cs
@@ -76,5 +76,123 @@ namespace TheDragonRuneTest
             QuickHPCheck(0, 0, -5, 0);
         }
 
+        [Test()]
+        public void TestEarthIllusion_PlayCard()
+        {
+            SetupGameController("BaronBlade", "TheDragonRune.Conman", "Legacy", "Bunker", "TheScholar", "Megalopolis");
+            StartGame();
+            DestroyNonCharacterVillainCards();
+            GoToPlayCardPhase(conman);
+            Card changeling = PlayCard("WindIllusion");
+            AssertInPlayArea(conman, changeling);
+            //When this card enters play shuffle all other Changeling cards into {Conman}’s deck."
+            QuickShuffleStorage(conman.TurnTaker.Deck);
+            Card earth = PlayCard("EarthIllusion");
+            AssertInDeck(changeling);
+            AssertInPlayArea(conman, earth);
+            QuickShuffleCheck(1);
+
+            //{Conman} may not Play card or use powers.
+            Card handCard = GetRandomCardFromHand(conman);
+            AssertCannotPlayCards(conman);
+            GoToUsePowerPhase(conman);
+            AssertCannotPerformPhaseAction();
+
+            //At the end of {Conman}’s turn, one other player may play a card or use a power.
+            DecisionSelectTurnTaker = bunker.TurnTaker;
+            Card playCard = PutInHand("AmmoDrop");
+            DecisionSelectCard = playCard;
+            DecisionSelectFunction = 0;
+            GoToEndOfTurn(conman);
+            AssertInPlayArea(bunker, playCard);
+
+            //At the start of {Conman}’s turn, you may shuffle this card into the deck.
+            DecisionYesNo = true;
+            QuickShuffleStorage(conman.TurnTaker.Deck);
+            GoToStartOfTurn(conman);
+            AssertInDeck(earth);
+            QuickShuffleCheck(1);
+
+        }
+
+        [Test()]
+        public void TestEarthIllusion_UsePower()
+        {
+            SetupGameController("BaronBlade", "TheDragonRune.Conman", "Legacy", "Bunker", "TheScholar", "Megalopolis");
+            StartGame();
+            DestroyNonCharacterVillainCards();
+            GoToPlayCardPhase(conman);
+            Card changeling = PlayCard("WindIllusion");
+            AssertInPlayArea(conman, changeling);
+            //When this card enters play shuffle all other Changeling cards into {Conman}’s deck."
+            QuickShuffleStorage(conman.TurnTaker.Deck);
+            Card earth = PlayCard("EarthIllusion");
+            AssertInDeck(changeling);
+            AssertInPlayArea(conman, earth);
+            QuickShuffleCheck(1);
+
+            //{Conman} may not Play card or use powers.
+            Card handCard = GetRandomCardFromHand(conman);
+            AssertCannotPlayCards(conman);
+            GoToUsePowerPhase(conman);
+            AssertCannotPerformPhaseAction();
+
+            //At the end of {Conman}’s turn, one other player may play a card or use a power.
+            DecisionSelectTurnTaker = bunker.TurnTaker;
+            DecisionSelectFunction = 1;
+            QuickHandStorage(bunker);
+            GoToEndOfTurn(conman);
+            QuickHandCheck(1);
+
+            //At the start of {Conman}’s turn, you may shuffle this card into the deck.
+            DecisionYesNo = true;
+            QuickShuffleStorage(conman.TurnTaker.Deck);
+            GoToStartOfTurn(conman);
+            AssertInDeck(earth);
+            QuickShuffleCheck(1);
+
+        }
+
+        [Test()]
+        public void TestEarthIllusion_Optional()
+        {
+            SetupGameController("BaronBlade", "TheDragonRune.Conman", "Legacy", "Bunker", "TheScholar", "Megalopolis");
+            StartGame();
+            DestroyNonCharacterVillainCards();
+            GoToPlayCardPhase(conman);
+            Card changeling = PlayCard("WindIllusion");
+            AssertInPlayArea(conman, changeling);
+            //When this card enters play shuffle all other Changeling cards into {Conman}’s deck."
+            QuickShuffleStorage(conman.TurnTaker.Deck);
+            Card earth = PlayCard("EarthIllusion");
+            AssertInDeck(changeling);
+            AssertInPlayArea(conman, earth);
+            QuickShuffleCheck(1);
+
+            //{Conman} may not Play card or use powers.
+            Card handCard = GetRandomCardFromHand(conman);
+            AssertCannotPlayCards(conman);
+            GoToUsePowerPhase(conman);
+            AssertCannotPerformPhaseAction();
+
+            //At the end of {Conman}’s turn, one other player may play a card or use a power.
+            DecisionSelectTurnTaker = bunker.TurnTaker;
+            Card playCard = PutInHand("AmmoDrop");
+            DecisionSelectCard = playCard;
+            DecisionDoNotSelectFunction = true;
+            QuickHandStorage(bunker);
+            GoToEndOfTurn(conman);
+            QuickHandCheckZero();
+            AssertInHand(playCard);
+
+            //At the start of {Conman}’s turn, you may shuffle this card into the deck.
+            DecisionYesNo = true;
+            QuickShuffleStorage(conman.TurnTaker.Deck);
+            GoToStartOfTurn(conman);
+            AssertInDeck(earth);
+            QuickShuffleCheck(1);
+
+        }
+
     }
 }
