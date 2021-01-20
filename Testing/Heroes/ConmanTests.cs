f858d1a972a63fff1b6309d3ac7508a4a7e5b086
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-01-19 19:49:48 -0600

    Created ConmanTests
    Added CustomBaseTestSuite

---
+++ Testing/Heroes/ConmanTests.cs
@@ -0,0 +1,38 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using Handelabra.Sentinels.UnitTest;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using TheDragonRune.Conman;
+
+namespace TheDragonRuneTest
+{
+    [TestFixture()]
+    public class ConmanTests : CustomBaseTest
+    {
+
+        private void SetupIncap(TurnTakerController villain)
+        {
+            SetHitPoints(conman.CharacterCard, 1);
+            DealDamage(villain, conman, 2, DamageType.Melee, true);
+        }
+
+        [Test()]
+        public void TestLoadConman()
+        {
+            SetupGameController("BaronBlade", "TheDragonRune.Conman", "Legacy", "Bunker", "TheScholar", "Megalopolis");
+            StartGame();
+            Assert.AreEqual(6, this.GameController.TurnTakerControllers.Count());
+
+            Assert.IsNotNull(conman);
+            Assert.IsInstanceOf(typeof(ConmanCharacterCardController), conman.CharacterCardController);
+
+            Assert.AreEqual(31, conman.CharacterCard.HitPoints);
+        }
+
+    }
+}
